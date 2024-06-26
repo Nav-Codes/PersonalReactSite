44dfb3df2fd96e77a62dce0fb11afb88bf2f7cb1
Navjot Kehal <navjotkehal5@gmail.com>
2024-06-26 12:40:54 -0700

    added about me section and navbar with links to my github, linkedin, apps page, and home page

---
+++ personal-react-project/src/Home.tsx
@@ -0,0 +1,21 @@
+import './Home.css'
+import Navbar from './Navbar.tsx'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="p-5 mb-4 bg-body-secondary rounded-3">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">About me</h1>
+          <img src="src/assets/profile-image.jpg" alt="screenshot" className="rounded float-end" id="about-me-image" />
+          <p className="col-md-8 fs-4">Hi there, I'm Navjot. I am a dedicated computer science student with a passion for software development,
+            constantly seeking to innovate and improve through coding. I am currently studying Computer Systems Technology at the
+            British Columbia Institute of Technology.</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ personal-react-project/src/Navbar.tsx
@@ -0,0 +1,29 @@
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Home</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/apps.html">Apps</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="https://github.com/Nav-Codes" target="_blank">GitHub</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="https://www.linkedin.com/in/navjot-kehal-a88b23212/" target="_blank">LinkedIn</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
